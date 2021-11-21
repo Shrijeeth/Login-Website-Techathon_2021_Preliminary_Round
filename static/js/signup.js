2221095dbaa3bbe83796e3aa3a83dcd961530092
Shrijeeth S <58306412+Shrijeeth@users.noreply.github.com>
2021-11-21 22:02:45 +0530

    Add files via upload

---
+++ static/js/signup.js
@@ -0,0 +1,22 @@
+/* Side navbar*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+/*Text hiding*/
+/*
+function show_text() {
+    document.getElementById('text2').style.visibility = "visible";
+}
+
+function hide_text() {
+    document.getElementById('text2').style.visibility = "hidden";
+}
+
+*/
