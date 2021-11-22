dd67f2d5edf8ad45587161b9485f5f8ac16050d8
Shrijeeth S <58306412+Shrijeeth@users.noreply.github.com>
2021-11-22 12:59:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, flash, session
 import sqlite3
-import os
 import hashlib
 import random
 
 app = Flask(__name__)
 app.secret_key = "#12345#"
 
+
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
+
+def check_credentials(cursor, value):
+    for row in cursor:
+        if row[0] == value:
+            return True
+    return False
 
 @app.route("/")
 def index():
@@ -19,13 +27,11 @@ def admin():
     if request.method == "POST":
         conn = sqlite3.connect("admin_login.db")
         username = request.form.get("uname")
-        password = request.form.get("pwd")
-        password = hashlib.md5(password.encode()).hexdigest()
+        password = hash_password(request.form.get("pwd"))
         cursor = conn.execute("SELECT username FROM login WHERE username=? AND password=?", (username, password))
-        for row in cursor:
-            if row[0] == username:
-                conn.close()
-                return render_template('success_admin.html')
+        if check_credentials(cursor, username):
+            conn.close()
+            return render_template('success_admin.html')
         conn.close()
         session['attempts'] += 1
         if session['attempts'] == session['random']:
@@ -41,13 +47,11 @@ def signin():
     if request.method == "POST":
         conn = sqlite3.connect("user_login.db")
         username = request.form.get("uname")
-        password = request.form.get("pwd")
-        password = hashlib.md5(password.encode()).hexdigest()
+        password = hash_password(request.form.get("pwd"))
         cursor = conn.execute("SELECT username FROM login WHERE username=? AND password=?", (username, password))
-        for row in cursor:
-            if row[0] == username:
-                conn.close()
-                return render_template('success_user.html')
+        if check_credentials(cursor, username):
+            conn.close()
+            return render_template('success_user.html', username=username)
         conn.close()
         session['attempts'] += 1
         if session['attempts'] == session['random']:
@@ -66,13 +70,10 @@ def signup():
         last_name = request.form.get("last_name").title()
         uname = request.form.get("uname")
         date_ = request.form.get("date_")
-        pwd = request.form.get("pwd")
-        pwd = hashlib.md5(pwd.encode()).hexdigest()
-        cpwd = request.form.get("cpwd")
-        cpwd = hashlib.md5(cpwd.encode()).hexdigest()
+        pwd = hash_password(request.form.get("pwd"))
+        cpwd = hash_password(request.form.get("cpwd"))
         security_question = request.form.get("security_question")
-        security_answer = request.form.get("security_answer").lower()
-        security_answer = hashlib.md5(security_answer.encode()).hexdigest()
+        security_answer = hash_password(request.form.get("security_answer").lower())
         if cpwd != pwd:
             conn.close()
             flash('Passwords does not match')
@@ -91,12 +92,9 @@ def password():
         conn = sqlite3.connect("user_login.db")
         uname = request.form.get("uname")
         security_question = request.form.get("security_question")
-        security_answer = request.form.get("security_answer").lower()
-        security_answer = hashlib.md5(security_answer.encode()).hexdigest()
-        pwd = request.form.get("pwd")
-        pwd = hashlib.md5(pwd.encode()).hexdigest()
-        cpwd = request.form.get("cpwd")
-        cpwd = hashlib.md5(cpwd.encode()).hexdigest()
+        security_answer = hash_password(request.form.get("security_answer").lower())
+        pwd = hash_password(request.form.get("pwd"))
+        cpwd = hash_password(request.form.get("cpwd"))
         if cpwd != pwd:
             conn.close()
             flash('Passwords does not match')
